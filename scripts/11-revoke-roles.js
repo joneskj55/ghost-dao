95d536a68540968f0588a637fa4d0693629b69ca
joneskj55 <joneskj55@gmail.com>
2021-12-22 09:13:11 -0600

    revoke roles; leave all priveleges to contract

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,27 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x15B4145D3f88fa3ef30220C0e05ac7fF42fB42E5"
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log(
+      "✅ Successfully revoked our superpowers from the ERC-20 contract"
+    );
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
